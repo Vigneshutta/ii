26861cf20667191ce77bb65447f3440dc74c76bc
Vigneshutta <gchitradevi97@gmail.com>
2017-07-10 17:37:36 +0530

    Create Fact.java

---
+++ Fact.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Fact
+{
+    public static void main(String args[])
+    {
+        int num, i, fact=1;
+        Scanner scan = new Scanner(System.in);
+    
+        System.out.print("Enter a Number : ");
+        num = scan.nextInt();
+    
+        for(i=num; i>0; i--)
+        {
+            fact = fact*i;
+        }
+    
+        System.out.print("Factorial of " + num + " is " + fact);
+    }
+}
